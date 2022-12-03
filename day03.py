78639194352e75832b102635a8b1e1281dffb4be
Oliver Merz <olli@oliver-merz.de>
2022-12-03 09:03:55 +0100

    changes to be more easy

--- day03.py
+++ day03.py
@@ -1,5 +1,4 @@
 from tools import config, log,helper
-from collections import Counter
 today="3"
 
 inputfile="input."+today
@@ -21,13 +20,11 @@ def puzzle_01():
         x=len(item)
         h=int(x/2)
         compartment1,compartment2=item[:h],item[h:]
-        c1=Counter(compartment1)
-        c2=Counter(compartment2)
 
-        commonDict = c1 & c2
-
+        commonDict = set(compartment1) & set(compartment2)
+       
         if len(commonDict) > 0:
-            commonChar = list(commonDict.elements())[0]
+            commonChar = list(commonDict)[0]
             p=prio(commonChar)
             result+=p
             
@@ -45,12 +42,12 @@ def puzzle_02():
         set2=inputdata[counter+1]
         set3=inputdata[counter+2]
         counter+=3
-        s1=Counter(set1)
-        s2=Counter(set2)
-        s3=Counter(set3)
+        s1=set(set1)
+        s2=set(set2)
+        s3=set(set3)
         commonDict=s1&s2&s3
 
-        c=list(commonDict.elements())[0]
+        c=list(commonDict)[0]
         p=prio(c)
         result+=p
 
