f54dab41d44d6e06ccff1c529a94f7f7eea643b7
dgxxx <119580907+dgxxx@users.noreply.github.com>
2022-12-06 09:38:18 +0000

    day06 done

--- day06.py
+++ day06.py
@@ -3,23 +3,52 @@ today="6"
 
 inputfile="input."+today
 
+def checkmarker(window):
+    res=False
+
+    return res
 def puzzle_01():
-    inputdata = helper.read_input(inputfile)
+    inputdata = helper.read_input(inputfile)[0]
     result=0
-    
-    for item in inputdata:
-        print(item)
+    c=0
+    #print(inputdata)
+    max=len(inputdata)-3
+    windowsize=4
+    while c<max:
+
+        window=inputdata[c:c+4]
+        #print(len(window))
+        #print(len(set(window)))
+        print("w: {} l: {} s: {}".format(window,len(window),len(set(window))))
+        if len(set(window))==windowsize:
+            print("found window: {}".format(window))
+            result=c+4
+            break
+        c+=1
     print("Puzzle-1: Result: {}".format(result))
 
 
 
 def puzzle_02():
 
-    inputdata = helper.read_input(inputfile)
+    inputdata = helper.read_input(inputfile)[0]
     result=0
+    c=0
+    windowsize=14
+    #print(inputdata)
+    max=len(inputdata)-14
+    while c<max:
+
+        window=inputdata[c:c+windowsize]
+        #print(len(window))
+        #print(len(set(window)))
+        print("w: {} l: {} s: {}".format(window,len(window),len(set(window))))
+        if len(set(window))==windowsize:
+            print("found window: {}".format(window))
+            result=c+windowsize
+            break
+        c+=1
     
-    for item in inputdata:
-        print(item)
     print("Puzzle-2: Result: {}".format(result))
 
 
